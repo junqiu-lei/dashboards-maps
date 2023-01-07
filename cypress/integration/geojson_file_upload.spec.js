3f1970dd15f162a88e1ee0274c058ff2f4f5e56f
Junqiu Lei <junqiu@amazon.com>
2023-01-07 00:00:13 -0800

    Unblock existing CI github actions
    Signed-off-by: Junqiu Lei <junqiu@amazon.com>

--- cypress/integration/geojson_file_upload.spec.js
+++ cypress/integration/geojson_file_upload.spec.js
@@ -11,15 +11,11 @@ import 'cypress-file-upload';
 describe('Verify successful custom geojson file upload', () => {
   before(() => {
     cy.visit(`${BASE_PATH}/app/home#/tutorial_directory/sampleData`);
-    // Click on "Sample data" tab
-    cy.contains('Sample data').click({ force: true });
-    // Load sample flights data
-    cy.get(`button[data-test-subj="addSampleDataSetflights"]`).click({
-      force: true,
-    });
-
-    // Verify that sample data is add by checking toast notification
-    cy.contains('Sample flight data installed', { timeout: 240000 });
+    cy.wait(5000)
+      .get('div[data-test-subj="sampleDataSetCardflights"]')
+      .contains(/(Add|View) data/)
+      .click();
+    cy.wait(10000);
 
     cy.visit(`${BASE_PATH}/app/visualize#/`);
 
@@ -40,11 +36,14 @@ describe('Verify successful custom geojson file upload', () => {
     cy.get('[data-testId="filePicker"]').attachFile('sample_geojson.json');
     cy.get('[data-testId="customIndex"]').type('sample');
     cy.contains('Import file').click({ force: true });
-    cy.contains('Successfully added 2 features to sample-map. Refresh to visualize the uploaded map.', { timeout: 240000 });
-  })
+    cy.contains(
+      'Successfully added 2 features to sample-map. Refresh to visualize the uploaded map.',
+      { timeout: 240000 }
+    );
+  });
 
   after(() => {
     cy.visit(`${BASE_PATH}/app/home#/tutorial_directory`);
     cy.get('button[data-test-subj="removeSampleDataSetflights"]').should('be.visible').click();
-  })
+  });
 });
--- cypress/integration/import_vector_map_tab.spec.js
+++ cypress/integration/import_vector_map_tab.spec.js
@@ -9,16 +9,11 @@ import { BASE_PATH } from '../utils/constants';
 
 describe('Verify the presence of import custom map tab in region map plugin', () => {
   before(() => {
-    cy.visit(`${BASE_PATH}/app/home#/tutorial_directory/sampleData`);
-    // Click on "Sample data" tab
-    cy.contains('Sample data').click({ force: true });
-    // Load sample flights data
-    cy.get(`button[data-test-subj="addSampleDataSetflights"]`).click({
-      force: true,
-    });
-
-    // Verify that sample data is add by checking toast notification
-    cy.contains('Sample flight data installed', { timeout: 240000 });
+    cy.wait(5000)
+      .get('div[data-test-subj="sampleDataSetCardflights"]')
+      .contains(/(Add|View) data/)
+      .click();
+    cy.wait(10000);
 
     cy.visit(`${BASE_PATH}/app/visualize#/`);
 
@@ -35,10 +30,10 @@ describe('Verify the presence of import custom map tab in region map plugin', ()
   it('checks import custom map tab is present', () => {
     // Click on "Import Vector Map" tab, which is part of customImportMap plugin
     cy.contains('Import Vector Map').click({ force: true });
-  })
+  });
 
   after(() => {
     cy.visit(`${BASE_PATH}/app/home#/tutorial_directory`);
     cy.get('button[data-test-subj="removeSampleDataSetflights"]').should('be.visible').click();
-  })
+  });
 });
