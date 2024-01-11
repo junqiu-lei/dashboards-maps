a096f9d469068de5a9af57e8de82b37b2f8a3648
Junqiu Lei <junqiu@amazon.com>
2024-01-11 11:20:42 -0800

    [2.x] Fix CI failure on maps-dashboards
    Signed-off-by: Junqiu Lei <junqiu@amazon.com>

--- cypress.config.js
+++ cypress.config.js
@@ -13,6 +13,9 @@ module.exports = defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
+    video: true,
+    experimentalMemoryManagement: true,
+    numTestsKeptInMemory: 2,
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
