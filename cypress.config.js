30d0ee2154b1e2629af359c788f66d4436eda68b
Junqiu Lei <junqiu@amazon.com>
2024-01-18 22:13:21 -0800

    Fix CI failure on maps-dashboards
    Signed-off-by: Junqiu Lei <junqiu@amazon.com>

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  video: true,
   defaultCommandTimeout: 120000,
   env: {
     opensearch_url: 'localhost:9200',
@@ -14,7 +15,7 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require('./cypress/plugins/index.js')(on, config);
     },
   },
-})
+});
